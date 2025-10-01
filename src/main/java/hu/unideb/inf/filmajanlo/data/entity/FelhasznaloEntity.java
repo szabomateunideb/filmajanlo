c1ed214f6914a1764032f4be82997d89e045b0f3
Máté <szabo.mate@inf.unideb.hu>
2025-10-01 12:45:07 +0200

    felhasznaloEntity, dbconfig

---
+++ src/main/java/hu/unideb/inf/filmajanlo/data/entity/FelhasznaloEntity.java
@@ -0,0 +1,37 @@
+package hu.unideb.inf.filmajanlo.data.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class FelhasznaloEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String nev;
+    @Column(name = "date_of_birth", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date szuletesiDatum;
+    @Column(name = "username", nullable = false, unique = true)
+    private String felhasznalonev;
+    @Column(name = "password", nullable = false)
+    private String jelszo;
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    @Column(name = "gender")
+    private String nem;
+    @Column(name = "country_code")
+    private String orszag;
+}
